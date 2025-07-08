95c944c5cdfcbd0cad29ab5c7b383f83fc92f896
Charles Felipe <charles.fmsilva@gmail.com>
2025-07-08 12:24:44 -0300

    OrderStatus enum

--- src/main/java/com/educandoweb/course/config/TesteConfig.java
+++ src/main/java/com/educandoweb/course/config/TesteConfig.java
@@ -12,6 +12,7 @@ import com.educandoweb.course.entities.Order;
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.OrderRepository;
 import com.educandoweb.course.repositories.UserRepository;
+import com.educandoweb.course.resources.enums.OrderStatus;
 
 @Configuration
 @Profile("test")
@@ -29,9 +30,9 @@ public class TesteConfig implements CommandLineRunner {
 		User u1 = new User(null, "Charles", "charles@gmail.com", "92863019", "17209");
 		User u2 = new User(null, "Henrique", "henrique@gmail.com", "60912788", "76140");
 		
-		Order o1 = new Order(null, Instant.parse("2025-06-20T19:53:00Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2025-07-09T12:29:41Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2025-06-27T15:09:18Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2025-06-20T19:53:00Z"), OrderStatus.DELIVERED, u1);
+		Order o2 = new Order(null, Instant.parse("2025-07-09T12:29:41Z"), OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2025-06-27T15:09:18Z"), OrderStatus.WAITING_PAYMENT, u1);
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
